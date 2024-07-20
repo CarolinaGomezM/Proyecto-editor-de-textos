a462f2866b0f4e25e87fab707bb314a8c4520e23
Carolina Edith Gómez Muñoz <89950310+CarolinaGomezM@users.noreply.github.com>
2024-07-19 20:50:53 -0600

    Delete tk.py

--- tk.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("Hola mundo")
-root.iconbitmap("hola.ico")
-root.resizable(True,True) # Si se quiere que no se pueda cambiar el tamaño de la ventana poner False, False
-root.mainloop()
-
